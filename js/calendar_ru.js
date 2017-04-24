35780f52c1bfdd9c864007dd7d722ebca16a80cc
Anzhelika Khivrenko <Ang.khivrenko@gmail.com>
2017-04-24 22:38:15 +0300

    Calendar pattern
    Addet pattern that describe calendar.

--- js/calendar_ru.js
+++ js/calendar_ru.js
@@ -55,3 +55,33 @@ function Top(obj)
 	return curtop;
 }
 
+// Calendar script
+var now = new Date;
+var sccd=now.getDate();
+var sccm=now.getMonth();
+var sccy=now.getFullYear();
+var ccm=now.getMonth();
+var ccy=now.getFullYear();
+
+// For current selected date
+var selectedd, selectedm, selectedy;
+
+document.write('<table id="fc" style="position:absolute;border-collapse:collapse;background:#FFFFFF;border:1px solid #FFD088;display:none;-moz-user-select:none;-khtml-user-select:none;user-select:none;" cellpadding="2">');
+document.write('<tr style="font:bold 13px Arial" onselectstart="return false"><td style="cursor:pointer;font-size:15px" onclick="upmonth(-1)">&laquo;</td><td colspan="5" id="mns" align="center"></td><td align="right" style="cursor:pointer;font-size:15px" onclick="upmonth(1)">&raquo;</td></tr>');
+document.write('<tr style="background:#FF9900;font:12px Arial;color:#FFFFFF"><td align=center>П</td><td align=center>В</td><td align=center>С</td><td align=center>Ч</td><td align=center>П</td><td align=center>С</td><td align=center>В</td></tr>');
+for(var kk=1;kk<=6;kk++) {
+	document.write('<tr>');
+	for(var tt=1;tt<=7;tt++) {
+		num=7 * (kk-1) - (-tt);
+		document.write('<td id="cv' + num + '" style="width:18px;height:18px">&nbsp;</td>');
+	}
+	document.write('</tr>');
+}
+document.write('<tr><td colspan="7" align="center" style="cursor:pointer;font:13px Arial;background:#FFC266" onclick="today()">Сегодня: '+addnull(sccd,sccm+1,sccy)+'</td></tr>');
+document.write('</table>');
+
+document.all?document.attachEvent('onclick',checkClick):document.addEventListener('click',checkClick,false);
+
+
+
+
