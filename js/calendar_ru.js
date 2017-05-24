5ba615b4ab654e62ab3b6039de27d881f31d5867
Ang.khivrenko <ang.khivrenko@gmail.com>
2017-05-24 09:06:47 +0300

    Update calendar_ru.js

--- js/calendar_ru.js
+++ js/calendar_ru.js
@@ -7,12 +7,14 @@
 function getObj(objID) {
     if (document.getElementById) {
         return document.getElementById(objID);
-    }
-    else if (document.all) {
-        return document.all[objID];
-    }
-    else if (document.layers) {
-        return document.layers[objID];
+    } else { 
+        if (document.all) {
+            return document.all[objID];
+        } else { 
+            if (document.layers) {
+                return document.layers[objID];
+            }
+        }
     }
 }
 /**
@@ -25,16 +27,20 @@ function getObj(objID) {
 function checkClick(e) {
     e?evt=e:evt=event;
     CSE=evt.target?evt.target:evt.srcElement;
-    if (CSE.tagName!='SPAN')
-	if (getObj('fc'))
-        if (!isChild(CSE,getObj('fc')))
-            getObj('fc').style.display='none';
+    if (CSE.tagName!='SPAN') {
+        if (getObj('fc')) {
+            if (!isChild(CSE,getObj('fc'))) {
+                getObj('fc').style.display='none';
+            }
+        }
+    }
 }
 
 function isChild(s,d) {
     while(s) {
-	    if (s==d)
-            return true;
+	    if (s==d) {
+            return true; 
+        }
         s=s.parentNode;
     }
     return false;
@@ -53,11 +59,14 @@ function left(obj) {
             curleft += obj.offsetLeft
             obj = obj.offsetParent;
         }
+    } else {
+        if (obj.x) {
+            curleft += obj.x; 
+        }
     }
-        else if (obj.x)
-            curleft += obj.x;
         return curleft;
 }
+
 function top(obj) {
     var curtop = 0;
     if (obj.offsetParent) {
@@ -65,9 +74,11 @@ function top(obj) {
             curtop += obj.offsetTop
             obj = obj.offsetParent;
         }
+    } else {
+        if (obj.y) { 
+            curtop += obj.y;
+        }
     }
-    else if (obj.y)
-        curtop += obj.y;
     return curtop;
 }
 /**
@@ -120,35 +131,44 @@ function lcs(ielem) {
         getObj('fc').style.top=Top(ielem)+ielem.offsetHeight+'px';
          getObj('fc').style.display='';
 
-	// First check date is valid
+    // First check date is valid
     curdt=ielem.value;
        curdtarr=curdt.split('-');
     isdt=true;
     for(var k=0;k<curdtarr.length;k++) {
-        if (isNaN(curdtarr[k]))
-            isdt=false;
+        if (isNaN(curdtarr[k])) {
+            isdt=false; 
+        }
     }
     if (isdt&(curdtarr.length==3)) {
-	        ccm=curdtarr[1]-1;
-	        ccy=curdtarr[2];
+        ccm=curdtarr[1]-1;
+        ccy=curdtarr[2];
 
-	        selectedd=parseInt ( curdtarr[0], 10 );
-                selectedm=parseInt ( curdtarr[1]-1, 10 );
-                selectedy=parseInt ( curdtarr[2], 10 );
+        selectedd=parseInt ( curdtarr[0], 10 );
+        selectedm=parseInt ( curdtarr[1]-1, 10 );
+        selectedy=parseInt ( curdtarr[2], 10 );
 
-                prepcalendar(curdtarr[0],curdtarr[1]-1,curdtarr[2]);
+        prepcalendar(curdtarr[0],curdtarr[1]-1,curdtarr[2]);
     }
 }
 
 function evtTgt(e) {
-        var el;
-        if(e.target)el=e.target;
-        else if(e.srcElement)el=e.srcElement;
-        if(el.nodeType==3)el=el.parentNode; // defeat Safari bug
-        return el;
+    var el;
+    if(e.target) {
+        el=e.target;
+    } else {
+        if(e.srcElement) {
+            el=e.srcElement;
+        }
+    if(el.nodeType==3) {
+        el=el.parentNode; // defeat Safari bug
+    }
+    return el;
 }
 function EvtObj(e) {
-    if(!e)e=window.event;
+    if(!e) {
+        e=window.event;
+    }
     return e;
 }
 function cs_over(e) {
@@ -178,31 +198,31 @@ var calvalarr=new Array(42);
 
 function f_cps(obj) {
     obj.style.background='#FFFFFF';
-          obj.style.font='10px Arial';
-          obj.style.color='#333333';
-          obj.style.textAlign='center';
-          obj.style.textDecoration='none';
-          obj.style.border='1px solid #FFD088';//'1px solid #606060';
-          obj.style.cursor='pointer';
+    obj.style.font='10px Arial';
+    obj.style.color='#333333';
+    obj.style.textAlign='center';
+    obj.style.textDecoration='none';
+    obj.style.border='1px solid #FFD088';//'1px solid #606060';
+    obj.style.cursor='pointer';
 }
 
 function f_cpps(obj) {
-          obj.style.background='#C4D3EA';
-          obj.style.font='10px Arial';
-          obj.style.color='#FF9900';
-          obj.style.textAlign='center';
-          obj.style.textDecoration='line-through';
-          obj.style.border='1px solid #6487AE';
-          obj.style.cursor='default';
+    obj.style.background='#C4D3EA';
+    obj.style.font='10px Arial';
+    obj.style.color='#FF9900';
+    obj.style.textAlign='center';
+    obj.style.textDecoration='line-through';
+    obj.style.border='1px solid #6487AE';
+    obj.style.cursor='default';
 }
 
 function f_hds(obj) {
-          obj.style.background='#FFF799';
-          obj.style.font='bold 10px Arial';
-          obj.style.color='#333333';
-          obj.style.textAlign='center';
-          obj.style.border='1px solid #6487AE';
-          obj.style.cursor='pointer';
+    obj.style.background='#FFF799';
+    obj.style.font='bold 10px Arial';
+    obj.style.color='#333333';
+    obj.style.textAlign='center';
+    obj.style.border='1px solid #6487AE';
+    obj.style.cursor='pointer';
 }
 /**
 * Выбор дня
@@ -214,14 +234,18 @@ function f_hds(obj) {
 */
 
 function prepcalendar(hd,cm,cy) {
-         now=new Date();
-         sd=now.getDate();
-         td=new Date();
-         td.setDate(1);
-         td.setFullYear(cy);
-         td.setMonth(cm);
+    now=new Date();
+    sd=now.getDate();
+    td=new Date();
+    td.setDate(1);
+    td.setFullYear(cy);
+    td.setMonth(cm);
     cd=td.getDay();
-    if (cd==0)cd=6; else cd--;
+    if (cd==0) {
+        cd=6; 
+    } else {
+        cd--;
+    }
     getObj('mns').innerHTML=mn[cm]+'&nbsp;<span style="cursor:pointer" onclick="upmonth(-12)">&lt;</span>'+cy+'<span style="cursor:pointer" onclick="upmonth(12)">&gt;</span>';
     marr=((cy%4)==0)?mnl:mnn;
     for(var d=1;d<=42;d++) {
@@ -235,35 +259,35 @@ function prepcalendar(hd,cm,cy) {
             cv.onmouseout=cs_out;
             cv.onclick=cs_click;
 
-			// if today
-                             if (sccm == cm && sccd == (d-cd) && sccy == cy)
-                             cv.style.color='#FF9900';
+            // if today
+            if (sccm == cm && sccd == (d-cd) && sccy == cy) {
+                cv.style.color='#FF9900';
+            }
 
-			// if selected date
+            // if selected date
             if (cm == selectedm && cy == selectedy && selectedd == (d-cd) ) {
                 cv.style.background='#FFEBCC';
-				//cv.style.color='#e0d0c0';
-				//cv.style.fontSize='1.1em';
-				//cv.style.fontStyle='italic';
-				//cv.style.fontWeight='bold';
+                //cv.style.color='#e0d0c0';
+                //cv.style.fontSize='1.1em';
+                //cv.style.fontStyle='italic';
+                //cv.style.fontWeight='bold';
 
-				// when use style.background
-                              cv.onmouseout=null;
+                // when use style.background
+                cv.onmouseout=null;
             }
-                            cv.innerHTML=d-cd;
-                            calvalarr[d]=addnull(d-cd,cm-(-1),cy);
-        }
-		else {
-                    cv.innerHTML='&nbsp;';
-                    cv.onmouseover=null;
-                    cv.onmouseout=null;
-                    cv.onclick=null;
-                    cv.style.cursor='default';
+                cv.innerHTML=d-cd;
+                calvalarr[d]=addnull(d-cd,cm-(-1),cy);
+        } else {
+            cv.innerHTML='&nbsp;';
+            cv.onmouseover=null;
+            cv.onmouseout=null;
+            cv.onclick=null;
+            cv.style.cursor='default';
         }
     }
-}
 
-prepcalendar('',ccm,ccy);
+    prepcalendar('',ccm,ccy);
+}
 
 /**
 * Функция подсчета месяцев
@@ -279,13 +303,14 @@ function upmonth(s)
     ccm+=s;
     if (ccm>=12) {
         ccm-=12;
-		ccy++;
-    }
-	else if(ccm<0) {
-              ccm+=12;
-              ccy--;
+        ccy++;
+    } else {
+        if(ccm<0) {
+          ccm+=12;
+          ccy--;
+        }
 	}
-             prepcalendar('',ccm,ccy);
+    prepcalendar('',ccm,ccy);
 }
 /**
 * Вызов текущей даты
@@ -303,8 +328,12 @@ function today() {
 
 function addnull(d,m,y) {
     var d0='',m0='';
-        if (d<10)d0='0';
-        if (m<10)m0='0';
+    if (d<10) {
+        d0='0';
+    }
+    if (m<10) {
+        m0='0';
+    }
 
-        return ''+d0+d+'-'+m0+m+'-'+y;
+    return ''+d0+d+'-'+m0+m+'-'+y;
 }
